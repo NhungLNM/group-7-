e52e50c6816b6db58bcb47dd88a655dfdcbf91f3
NhungLNM <10323056@student.vgu.edu.vn>
2024-06-30 14:42:52 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -38,3 +38,9 @@ if not filtered_data['Medal'].dropna().empty:
     st.plotly_chart(fig_pie)
 else:
     st.warning(f"No medal data available for {sport}")
+
+# Plotting - Line Graph for Number of Athletes over the Years
+st.header("Number of Athletes Over the Years")
+year_counts = filtered_data['Year'].value_counts().sort_index()
+fig_line = px.line(x=year_counts.index, y=year_counts.values, labels={'x':'Year', 'y':'Number of Athletes'}, title=f'Number of Athletes Over the Years in {sport} from {country}')
+st.plotly_chart(fig_line)
